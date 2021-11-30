341d492de6e1935de2b1261cf60357880780ba59
polarbear23 <xkyd96@gmail.com>
2021-11-30 18:42:33 +0000

    Finished base task-added toggle read and toggle star functions

--- src/app.js
+++ src/app.js
@@ -1,12 +1,38 @@
-import Header from './components/header'
+import Header from "./components/header";
+import React, { useState } from "react";
+import initialEmails from "./data/emails";
 
-import initialEmails from './data/emails'
-
-import './styles/app.css'
+import "./styles/app.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  console.log(initialEmails);
+  const [emails, setEmails] = useState(initialEmails);
+  const toggleRead = (e) => {
+    const index = emails.findIndex((email) => {
+      return email.id.toString() === e.target.id;
+    });
+    const newEmails = [...emails];
+    if (index > -1) {
+      console.log(newEmails);
+      console.log(index);
+      newEmails[index].read = !newEmails[index].read;
+      setEmails(newEmails);
+    }
+  };
+
+  const toggleStar = (e) => {
+    const index = emails.findIndex((email) => {
+      return email.id.toString() === e.target.id;
+    });
+    const newEmails = [...emails];
+    if (index > -1) {
+      console.log(newEmails);
+      console.log(index);
+      newEmails[index].starred = !newEmails[index].starred;
+      setEmails(newEmails);
+    }
+  };
 
   return (
     <div className="app">
@@ -33,15 +59,40 @@ function App() {
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
+              defaultChecked={false}
               // onChange={() => {}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul className="emails">
+          {emails.map((email) => (
+            <li
+              className={`email ${email.read ? "read" : "unread"}`}
+              key={email.id}
+            >
+              <input
+                id={email.id}
+                type="checkbox"
+                name="emailcheck"
+                defaultChecked={email.read}
+                onChange={toggleRead}
+              />
+              <input
+                type="checkbox"
+                className="star-checkbox"
+                name="star"
+                defaultChecked={email.starred}
+              />
+              <span>{email.sender}</span>
+              <span>{email.title}</span>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
